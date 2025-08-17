a66edc797a2b8998c6574f2e17f39c452baa92f3
Muhammad Bilal <muhammadbilaal098@gmail.com>
2025-08-17 13:49:19 +0500

    commit

--- client/src/components/auth/ForgotPassword.tsx
+++ client/src/components/auth/ForgotPassword.tsx
@@ -51,7 +51,7 @@ export default function ForgotPassword() {
         return;
       }
 
-      const response = await fetch("/api/auth/forgot-password", {
+      const response = await fetch("/api/forget-password", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email }),
